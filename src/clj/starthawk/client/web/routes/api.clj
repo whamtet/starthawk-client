b3b5cf897c68f4e68d9ce46d397800f02378ecda
Matthew Molloy <whamtet@gmail.com>
2025-05-12 23:58:56 +0900

    better status update

--- src/clj/starthawk/client/web/routes/api.clj
+++ src/clj/starthawk/client/web/routes/api.clj
@@ -45,10 +45,7 @@
        :headers {"Content-Type" "text/plain"}
        :body (starthawk/refresh-session!)})]
    ["/status/:id"
-    (fn [req]
-      {:status 200
-       :headers {"Content-Type" "text/html"}
-       :body (-> req :path-params :id views.starthawk/status)})]
+    views.starthawk/status]
    ["/health"
     ;; note that use of the var is necessary
     ;; for reitit to reload routes without
--- src/clj/starthawk/client/web/views/starthawk.clj
+++ src/clj/starthawk/client/web/views/starthawk.clj
@@ -3,5 +3,33 @@
       [hiccup2.core :as h]
       [starthawk.client.web.controllers.starthawk :as starthawk]))
 
-(defn status [id]
-  (starthawk/get-status id))
+(def html #(-> % h/html str))
+
+(defn plain-response [body]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body body})
+
+(defn retarget-response [id body]
+  {:status 200
+   :headers {"Content-Type" "text/html"
+             "HX-Reswap" "outerHTML"
+             "HX-Retarget" (str "#" id)}
+   :body (html body)})
+
+(defn status [{:keys [params path-params]}]
+  (let [{:keys [id]} path-params
+        {:keys [profilePic]} params
+        status (starthawk/get-status id)]
+    (if (= "Not very active" status)
+      (retarget-response
+       id
+       [:div {:style "padding: 4px; margin-bottom: 4px"
+              :id id}
+        [:div {:style "margin-bottom: 4px, display: flex"}
+         [:a {:href (format "https://www.starthawk.io/profile/%s" id)
+              :target "_blank"}
+          [:img {:style {:width "64px" :margin-right "1rem"}
+                 :src profilePic}]]]
+        [:hr]])
+      (plain-response status))))
